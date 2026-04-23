13309a11733022c309687b4f10c32951ea7e64c7
LeNhutQuan-2331200091 <quan.le.cit23@eiu.edu.vn>
2026-04-23 08:55:33 +0700

    Test: Implement integration tests for Controller-Model interaction

--- tests/integration/controller.test.js
+++ tests/integration/controller.test.js
@@ -1,5 +1,5 @@
-const { TodoService } = require('../../js/model');
-const { Controller } = require('../../js/controller');
+const { TodoService } = require("../../js/model");
+const { Controller } = require("../../js/controller");
 
 // Mock the View because we are not testing the UI, only Controller-Model interaction.
 const mockView = {
@@ -9,7 +9,7 @@ const mockView = {
     bindRemoveTodo: jest.fn(),
 };
 
-describe('Controller-Service Integration Tests', () => {
+describe("Controller-Service Integration Tests", () => {
     let service;
     let controller;
 
@@ -19,17 +19,24 @@ describe('Controller-Service Integration Tests', () => {
         controller = new Controller(service, mockView);
     });
 
-    test('handleAddTodo should call service.addTodo and update the model', () => {
+    test("handleAddTodo should call service.addTodo and update the model", () => {
         // TODO: Call the controller's handleAddTodo method with some test text.
         // Then, get the list of todos directly from the service.
         // Assert that the service's todos array has a length of 1.
         // Assert that the text of the first todo in the service matches the input.
+        controller.handleAddTodo("Test Todo");
+        expect(service.todos).toHaveLength(1);
+        expect(service.todos[0].text).toBe("Test Todo");
     });
 
-    test('handleRemoveTodo should call service.removeTodo and update the model', () => {
+    test("handleRemoveTodo should call service.removeTodo and update the model", () => {
         // TODO: First, directly add a todo to the service.
         // Get the ID of the new todo.
         // Call the controller's handleRemoveTodo method with that ID.
         // Assert that the service's todos array is now empty.
+        controller.handleAddTodo("Test Todo to Remove");
+        const todo = service.todos[0];
+        controller.handleRemoveTodo(todo.id);
+        expect(service.todos).toHaveLength(0);
     });
 });
